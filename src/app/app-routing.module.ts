a7f7790f0b4c40adb876074d4a66d6baa75e0831
shaon@live.com <Tt4to202./>
2020-04-10 15:26:04 +0600

    add registration page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,10 @@ const routes: Routes = [
     path: 'login',
     loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
   },
+  {
+    path: 'registration',
+    loadChildren: () => import('./pages/registration/registration.module').then( m => m.RegistrationPageModule)
+  },
 ];
 
 @NgModule({
